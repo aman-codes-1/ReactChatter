2c81538c2aa346d602170cf0ccbd8a4cc64748b0
aman-codes-1 <aman.codes0@gmail.com>
2025-03-07 21:51:33 +0530

    fixed MessageData and Receiver types

--- src/contexts/ChatsAndFriends/IData.ts
+++ src/contexts/ChatsAndFriends/IData.ts
@@ -1,7 +1,7 @@
 type RetryStatus = {
   isRetry: boolean;
   timestamp: number;
-} | null;
+};
 
 type QueuedStatus = {
   isQueued: boolean;
@@ -11,38 +11,38 @@ type QueuedStatus = {
 type SentStatus = {
   isSent: boolean;
   timestamp: number;
-} | null;
+};
 
 type DeliveredStatus = {
   isDelivered: boolean;
   timestamp: number;
-} | null;
+};
 
 type ReadStatus = {
   isRead: boolean;
   timestamp: number;
-} | null;
+};
 
 type Sender = {
   _id: string;
-  retryStatus: RetryStatus;
+  retryStatus?: RetryStatus;
   queuedStatus: QueuedStatus;
   sentStatus: SentStatus;
 };
 
 export type Receiver = {
   _id: string;
-  deliveredStatus: DeliveredStatus;
-  readStatus: ReadStatus;
+  deliveredStatus?: DeliveredStatus;
+  readStatus?: ReadStatus;
 };
 
 export type MessageData = {
   _id: string;
-  queueId: string;
   chatId: string;
-  isActive?: boolean;
+  queueId: string;
+  isActive: boolean;
   message: string;
   sender: Sender;
-  receivers?: Receiver[];
+  receivers: Receiver[];
   timestamp: number;
 };

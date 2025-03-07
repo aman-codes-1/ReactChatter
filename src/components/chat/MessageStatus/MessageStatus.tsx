b25ca0047ad6d194074aba44a1ba2188f7be1e46
aman-codes-1 <aman.codes0@gmail.com>
2025-03-07 17:03:30 +0530

    added conditional rendering in MessageStatus which wasn't working properly, added defaultColor

--- src/components/chat/MessageStatus/MessageStatus.tsx
+++ src/components/chat/MessageStatus/MessageStatus.tsx
@@ -2,58 +2,52 @@ import { useTheme } from '@mui/material';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import DoneRoundedIcon from '@mui/icons-material/DoneRounded';
 import DoneAllRoundedIcon from '@mui/icons-material/DoneAllRounded';
-import { checkMessageStatus } from '../../../helpers';
 
-const MessageStatus = ({
-  msg,
-  messageStatus,
-  selectedChat,
-  color,
-  readColor,
-}: any) => {
+const MessageStatus = ({ messageStatus, defaultColor, readColor }: any) => {
   const theme = useTheme();
+  const { isSent, isDelivered, isRead } = messageStatus || {};
 
-  const { isQueued, isSent, isDelivered, isRead } =
-    (msg && selectedChat
-      ? checkMessageStatus(msg, selectedChat)
-      : messageStatus) || {};
-
-  return (
-    <>
-      {isQueued ? (
-        <AccessTimeIcon
-          fontSize="inherit"
-          sx={{
-            color: color || theme.palette.text.secondary,
-          }}
-        />
-      ) : null}
-      {isSent ? (
-        <DoneRoundedIcon
-          fontSize="inherit"
-          sx={{
-            color: color || theme.palette.text.secondary,
-          }}
-        />
-      ) : null}
-      {isDelivered ? (
-        <DoneAllRoundedIcon
-          fontSize="inherit"
-          sx={{
-            color: color || theme.palette.text.secondary,
-          }}
-        />
-      ) : null}
-      {isRead ? (
+  const renderStatus = () => {
+    if (isRead) {
+      return (
         <DoneAllRoundedIcon
           fontSize="inherit"
           sx={{
             color: readColor || theme.palette.primary.main,
           }}
         />
-      ) : null}
-    </>
-  );
+      );
+    } else if (isDelivered) {
+      return (
+        <DoneAllRoundedIcon
+          fontSize="inherit"
+          sx={{
+            color: defaultColor || theme.palette.text.secondary,
+          }}
+        />
+      );
+    } else if (isSent) {
+      return (
+        <DoneRoundedIcon
+          fontSize="inherit"
+          sx={{
+            color: defaultColor || theme.palette.text.secondary,
+          }}
+        />
+      );
+    } else {
+      return (
+        <AccessTimeIcon
+          fontSize="inherit"
+          sx={{
+            color: defaultColor || theme.palette.text.secondary,
+          }}
+        />
+      );
+    }
+  };
+
+  return <>{renderStatus()}</>;
 };
 
 export default MessageStatus;

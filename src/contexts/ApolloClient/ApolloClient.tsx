6d3f0a507cc614029a4822625268402558134601
aman-codes-1 <aman.codes0@gmail.com>
2025-03-07 21:44:07 +0530

    add states such as isWsConnecting, isWsConnected, isWsError, isNetworkError in ApolloClient context

--- src/contexts/ApolloClient/ApolloClient.tsx
+++ src/contexts/ApolloClient/ApolloClient.tsx
@@ -6,20 +6,36 @@ import { createApolloClient } from './createApolloClient';
 export const ApolloClientContext = createContext<any>({});
 
 export const ApolloClientProvider = ({ children }: any) => {
+  const [isWsConnecting, setIsWsConnecting] = useState(true);
   const [isWsConnected, setIsWsConnected] = useState(false);
+  const [isWsError, setIsWsError] = useState(false);
+  const [isNetworkError, setIsNetworkError] = useState(false);
   const { auth } = useAuth();
   const { callLogout } = useApi();
 
   const client: any = useMemo(
-    () => createApolloClient(callLogout, setIsWsConnected),
+    () =>
+      createApolloClient(
+        callLogout,
+        setIsWsConnecting,
+        setIsWsConnected,
+        setIsWsError,
+        setIsNetworkError,
+      ),
     [auth?.isLoggedIn],
   );
 
   return (
     <ApolloClientContext.Provider
       value={{
+        isWsConnecting,
+        setIsWsConnecting,
         isWsConnected,
         setIsWsConnected,
+        isWsError,
+        setIsWsError,
+        isNetworkError,
+        setIsNetworkError,
       }}
     >
       <ApolloProvider client={client}>{children}</ApolloProvider>

6bb4d258a19b81d06e4743012e2f242d9482c559
aman-codes-1 <aman.codes0@gmail.com>
2025-03-07 17:06:40 +0530

    fixed generic Mui Drawer, fixed and added new props for better rendering, fixed its styling

--- src/components/nav/Drawer/Drawer.tsx
+++ src/components/nav/Drawer/Drawer.tsx
@@ -1,36 +1,38 @@
-import { Drawer as MuiDrawer, useTheme } from '@mui/material';
+import { forwardRef, useContext } from 'react';
+import { DrawerProps, Drawer as MuiDrawer, useTheme } from '@mui/material';
+import { DrawerContext } from '../../../contexts';
 
-const Drawer = ({
-  anchor,
-  isOpen,
-  onClose,
-  isMobile,
-  navbarHeight = 0,
-  children,
-}: any) => {
+const Drawer = forwardRef<HTMLDivElement, DrawerProps>((props, ref) => {
+  const { children, ...rest } = props;
+
   const theme = useTheme();
+  const { navBarHeight, getDrawerWidth } = useContext(DrawerContext);
 
   return (
     <MuiDrawer
-      anchor={anchor}
-      open={isOpen}
-      onClose={onClose}
-      sx={
-        isMobile
-          ? {
-              [theme.breakpoints.up('sm')]: {
-                display: 'none',
-              },
-            }
-          : {}
-      }
-      PaperProps={{
-        sx: { width: '90vw', height: `calc(100% - ${navbarHeight || 0}px)` },
+      {...rest}
+      ref={ref}
+      sx={{
+        width: '100%',
+        maxWidth: `${getDrawerWidth() || 300}px`,
+        flexShrink: 0,
+      }}
+      slotProps={{
+        paper: {
+          sx: {
+            width: '100%',
+            maxWidth: `${getDrawerWidth() || 300}px`,
+            height: `calc(100% - ${navBarHeight || 0}px)`,
+            [theme.breakpoints.down('xs')]: {
+              maxWidth: '82vw',
+            },
+          },
+        },
       }}
     >
       {children}
     </MuiDrawer>
   );
-};
+});
 
 export default Drawer;
